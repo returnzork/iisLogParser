05dbec3e4e9746b196f480e3743c3c16a92a94f3
K Z <hellowatex@gmail.com>
2020-08-08 21:40:38 -0700

    Basic parsing

--- IIS Log Parser/Program.cs
+++ IIS Log Parser/Program.cs
@@ -1,11 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace IIS_Log_Parser
+namespace returnzork.IIS_Log_Parser
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a IIS Log file to parse");
+            string file = Console.ReadLine();
+
+            if(!File.Exists(file))
+            {
+                Console.WriteLine("File does not exist");
+            }
+            else
+            {
+                FileWork(file);
+            }
+        }
+
+        static void FileWork(string file)
+        {
+            var lines = ReadFile(file);
+            Console.WriteLine($"There were a total of {lines.Count} log entries");
+        }
+
+        static List<string> ReadFile(string file)
+        {
+            List<string> lines = new List<string>();
+            using (StreamReader sr = new StreamReader(file))
+            {
+                while(!sr.EndOfStream)
+                {
+                    lines.Add(sr.ReadLine());
+                }
+            }
+
+            return lines;
         }
     }
 }
