284ddebbfe929b06d223d25067657a2ed63b4d26
K Z <hellowatex@gmail.com>
2020-08-24 17:00:21 -0700

    Move the functions of the switch statement to their own methods

--- IIS Log Parser/Logic.cs
+++ IIS Log Parser/Logic.cs
@@ -10,17 +10,18 @@ namespace returnzork.IIS_Log_Parser
     internal class Logic
     {
         private List<ILogItem> logs;
+        private LogDisplay display;
 
         internal Logic(List<ILogItem> logs)
         {
             this.logs = logs;
+            display = new LogDisplay(logs);
         }
 
         internal void Run()
         {
             //setup requirements
             MenuEntry entry;
-            LogDisplay display = new LogDisplay(logs);
             bool shouldExit = false;
 
 
@@ -37,87 +38,17 @@ namespace returnzork.IIS_Log_Parser
                         break;
 
                     case MenuEntry.AddLogFile:
-                        Console.WriteLine("Enter new log file to add");
-                        string newFile = Console.ReadLine();
-                        if (!File.Exists(newFile))
-                        {
-                            Console.WriteLine("File does not exist");
-                        }
-                        else
-                        {
-                            logs.AddRange(Program.ParseLines(Program.ReadFile(newFile)));
-                            display = new LogDisplay(logs);
-                        }
+                        AddLogFile();
                         break;
                     case MenuEntry.LoadFolder:
-                        Console.WriteLine("Enter folder to load from");
-                        string dir = Console.ReadLine();
-                        if (!Directory.Exists(dir))
-                        {
-                            Console.WriteLine("Directory does not exist");
-                        }
-                        else
-                        {
-                            logs = Program.LoadDirectory(dir);
-                            display = new LogDisplay(logs);
-                        }
+                        LoadLogFolder();
                         break;
 
                     case MenuEntry.GlobalIgnore:
-                        //global ignore
-                        Console.WriteLine("Enter [ip, address] to ignore (MINIMUM 2 ENTRIES):");
-                        string arr = Console.ReadLine();
-                        if (!Helper.IpSplit(arr, out string[] split))
-                        {
-                            Console.WriteLine("Invalid format");
-                        }
-                        else
-                        {
-                            for (int i = logs.Count - 1; i >= 0; i--)
-                            {
-                                if (split.Contains(logs[i].ClientIpAddr))
-                                    logs.RemoveAt(i);
-                            }
-                        }
+                        AddGlobalIgnore();
                         break;
                     case MenuEntry.GlobalIgnoreFile:
-                        Console.WriteLine("Enter file containing [ip, address] to ignore:");
-                        string file = Console.ReadLine();
-                        if (!File.Exists(file))
-                        {
-                            Console.WriteLine("File does not exist");
-                        }
-                        else
-                        {
-                            using (StreamReader sr = new StreamReader(file))
-                            {
-                                //cast the list to an array so we can use a parallel for loop
-                                var logArray = logs.ToArray();
-
-                                while (!sr.EndOfStream)
-                                {
-                                    string line = sr.ReadLine();
-                                    if (string.IsNullOrEmpty(line))
-                                        continue;
-
-                                    if (!Helper.IpSplit(line, out string[] res))
-                                    {
-                                        Console.WriteLine("Invalid format");
-                                    }
-                                    else
-                                    {
-                                        System.Threading.Tasks.Parallel.For(0, logArray.Length, (i) =>
-                                        {
-                                            if (res.Contains(logArray[i]?.ClientIpAddr))
-                                                logArray[i] = default;
-                                        });
-                                    }
-                                }
-
-                                logs = logArray.Where(x => x != null && x.IsValid).ToList();
-                                display = new LogDisplay(logs);
-                            }
-                        }
+                        AddGlobalIgnoreFile();
                         break;
 
 
@@ -144,6 +75,95 @@ namespace returnzork.IIS_Log_Parser
                 }
             }
             while (entry != MenuEntry.Exit && !shouldExit);
+        }
+
+        private void AddGlobalIgnoreFile()
+        {
+            Console.WriteLine("Enter file containing [ip, address] to ignore:");
+            string file = Console.ReadLine();
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("File does not exist");
+            }
+            else
+            {
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    //cast the list to an array so we can use a parallel for loop
+                    var logArray = logs.ToArray();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        if (string.IsNullOrEmpty(line))
+                            continue;
+
+                        if (!Helper.IpSplit(line, out string[] res))
+                        {
+                            Console.WriteLine("Invalid format");
+                        }
+                        else
+                        {
+                            System.Threading.Tasks.Parallel.For(0, logArray.Length, (i) =>
+                            {
+                                if (res.Contains(logArray[i]?.ClientIpAddr))
+                                    logArray[i] = default;
+                            });
+                        }
+                    }
+
+                    logs = logArray.Where(x => x != null && x.IsValid).ToList();
+                    display = new LogDisplay(logs);
+                }
+            }
+        }
+
+        private void AddGlobalIgnore()
+        {
+            Console.WriteLine("Enter [ip, address] to ignore (MINIMUM 2 ENTRIES):");
+            string arr = Console.ReadLine();
+            if (!Helper.IpSplit(arr, out string[] split))
+            {
+                Console.WriteLine("Invalid format");
+            }
+            else
+            {
+                for (int i = logs.Count - 1; i >= 0; i--)
+                {
+                    if (split.Contains(logs[i].ClientIpAddr))
+                        logs.RemoveAt(i);
+                }
+            }
+        }
+
+        private void LoadLogFolder()
+        {
+            Console.WriteLine("Enter folder to load from");
+            string dir = Console.ReadLine();
+            if (!Directory.Exists(dir))
+            {
+                Console.WriteLine("Directory does not exist");
+            }
+            else
+            {
+                logs = Program.LoadDirectory(dir);
+                display = new LogDisplay(logs);
+            }
+        }
+
+        private void AddLogFile()
+        {
+            Console.WriteLine("Enter new log file to add");
+            string newFile = Console.ReadLine();
+            if (!File.Exists(newFile))
+            {
+                Console.WriteLine("File does not exist");
+            }
+            else
+            {
+                logs.AddRange(Program.ParseLines(Program.ReadFile(newFile)));
+                display = new LogDisplay(logs);
+            }
         }
     }
 }
