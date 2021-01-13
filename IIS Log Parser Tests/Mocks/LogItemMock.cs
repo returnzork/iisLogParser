3bcbc17eb6740a11e1c9eed99a6e596368240aa8
K Z <hellowatex@gmail.com>
2021-01-12 21:34:09 -0800

    Add documentation to LogItemMock.cs

--- IIS Log Parser Tests/Mocks/LogItemMock.cs
+++ IIS Log Parser Tests/Mocks/LogItemMock.cs
@@ -10,14 +10,26 @@ namespace returnzork.IIS_Log_Parser_Tests.Mocks
     {
         static Type logItemType;
 
+        /// <summary>
+        /// Create a ILogItem entry
+        /// </summary>
+        /// <param name="clientIp">Ip address to set (ex 127.0.0.1)</param>
+        /// <param name="verb">Http verb (ex Get)</param>
+        /// <param name="status">Http status code (ex 500)</param>
+        /// <param name="path">Path client requested (ex /folder1/folder2/)</param>
+        /// <param name="file">File client requested (ex myfile.txt)</param>
+        /// <returns>ILogItem with specified parameters</returns>
         internal static ILogItem GetGenericLog(string clientIp, string verb, int status, string path = "", string file = "TestLogFile.txt")
         {
+            //reflection loading
             if(logItemType == null)
                 logItemType = Type.GetType("returnzork.IIS_Log_Parser.LogItem, IIS Log Parser");
 
+            //if the path the user specified ends with a forwardslash, remove it
             if (path.EndsWith('/'))
                 path = path.Substring(0, path.Length - 1);
 
+            //set the values of the ILogItem with what the user specified
             DateTime Time = DateTime.Now;
             string ServerIpAddr = "127.0.0.1";
             string HTTPVerb = verb;
@@ -33,9 +45,10 @@ namespace returnzork.IIS_Log_Parser_Tests.Mocks
             string WindowsStatus = "0";
             TimeSpan TimeTaken = new TimeSpan(0);
 
-            ILogItem instance = LogItem.Create(new string[] { $"{Time.Year}-{Time.Month}-{Time.Day}", $"{Time.Hour}:{Time.Minute}:{Time.Second}", ServerIpAddr, HTTPVerb, Uri, Query, Port.ToString(), Username, 
+            //reflection instantiation of item
+            ILogItem instance = LogItem.Create(new string[] { $"{Time.Year}-{Time.Month}-{Time.Day}", $"{Time.Hour}:{Time.Minute}:{Time.Second}", ServerIpAddr, HTTPVerb, Uri, Query, Port.ToString(), Username,
                 ClientIpAddr, UserAgent, Referer, HTTPStatus.ToString(), HTTPSubStatus.ToString(), WindowsStatus, TimeTaken.TotalMilliseconds.ToString() });
-            return (ILogItem)instance;
+            return instance;
         }
     }
 }
